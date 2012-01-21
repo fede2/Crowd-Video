0e75fef123ca567032ff6e65ecde72a90e84f52d
Alvaro Figueroa C <alvaro.figueroa.cabezas@gmail.com>
2012-01-20 20:26:56 -0600

    Added README, TODO and mindmap.

---
+++ Crowd Video.mm
@@ -0,0 +1,142 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1303424502057" ID="ID_27917392" MODIFIED="1303424520723" TEXT="Crowd Video">
+<node CREATED="1303424524084" ID="ID_602571318" MODIFIED="1303424527429" POSITION="right" TEXT="Inspiraci&#xf3;n">
+<node CREATED="1303424588861" ID="ID_1792514990" MODIFIED="1303424596948" TEXT="Video de Radiohead en Praga">
+<node CREATED="1303424603322" ID="ID_1240314561" MODIFIED="1303424611770" TEXT="Radiohead pone el audio de consola"/>
+<node CREATED="1303424612258" ID="ID_93593685" MODIFIED="1303424623141" TEXT="Los asistentes al concierto ponen videos de cualquier medio"/>
+<node CREATED="1303424623606" ID="ID_491726127" MODIFIED="1303424683296" TEXT="Los fan&#xe1;ticos de la banda editan los videos contribu&#xed;dos, para unirlo con el audio de consola"/>
+</node>
+</node>
+<node CREATED="1303424528286" ID="ID_101276079" MODIFIED="1303424531767" POSITION="right" TEXT="Prop&#xf3;sito">
+<node CREATED="1303424767377" ID="ID_712871617" MODIFIED="1303424886298" TEXT="Crear un conjunto de herramientas de software y gu&#xed;as de uso, con el fin de que cualquier grupo de fans grabe un concierto de su banda preferida, siempre y cuando la banda contribuya el audio">
+<node CREATED="1303424889048" ID="ID_91707188" MODIFIED="1303424934880" TEXT="Sumamente &#xfa;til para grupos sin apoyo de disqueras"/>
+</node>
+</node>
+<node CREATED="1303424548053" ID="ID_1422526963" MODIFIED="1303424557289" POSITION="right" TEXT="Detalles t&#xe9;cnicos">
+<node CREATED="1303424935530" ID="ID_318209496" MODIFIED="1303424938437" TEXT="Software">
+<node CREATED="1303424949104" FOLDED="true" ID="ID_185973809" MODIFIED="1327107977888" TEXT="Crowd-video">
+<node CREATED="1303424958864" ID="ID_1617709603" MODIFIED="1303425014141" TEXT="Interface web que ayuda a sugerir a los editores finales, las porciones de video que pueden ser utilizados"/>
+<node CREATED="1303425022233" ID="ID_1587201641" MODIFIED="1303425058854" TEXT="Utiliza videos de youtube para la etapa de edici&#xf3;n preliminar, y los originales constribu&#xed;dos para realizar la edici&#xf3;n final">
+<node CREATED="1303425065540" ID="ID_259393129" MODIFIED="1303425095684" TEXT="Permite eliminar los detalles de distribuci&#xf3;n del contenido de multimedia"/>
+<node CREATED="1303425096585" ID="ID_1220788496" MODIFIED="1303425108256" TEXT="Probablemente ya ha sido utilizado por otros proyectos">
+<node CREATED="1303425110470" ID="ID_807401128" MODIFIED="1303425115056" TEXT="Coro TED">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1303425123844" ID="ID_1002253931" MODIFIED="1303425149860" TEXT="Requiere que el grupo de personas que reciben los videos, los suban a youtube"/>
+</node>
+<node CREATED="1303425176512" ID="ID_207974047" MODIFIED="1303425178259" TEXT="Procesos">
+<node CREATED="1303425185823" ID="ID_1623536355" MODIFIED="1303425195656" TEXT="Creaci&#xf3;n de proyecto">
+<node CREATED="1303425242204" ID="ID_593870159" MODIFIED="1303425250175" TEXT="Permiso escrito de la banda">
+<node CREATED="1303425270613" ID="ID_757326215" MODIFIED="1303425332992" TEXT="Estipulando que dona el audio bajo licencia CC o que hace release del audio bajo licencia CC"/>
+</node>
+<node CREATED="1303425341442" ID="ID_848343900" MODIFIED="1303425370172" TEXT="Recepci&#xf3;n de contribuyentes">
+<node CREATED="1303425373250" ID="ID_220043172" MODIFIED="1303425451480" TEXT="Las etapas de prueba van a determinar la cantidad de contribuyentes necesarios para un video decente">
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1303425826856" ID="ID_976975764" MODIFIED="1303425847338" TEXT="Se habla con el grupo para que transmita la idea por Facebook para que la gente se registre al proyecto"/>
+</node>
+</node>
+<node CREATED="1303425181086" ID="ID_725457350" MODIFIED="1303425184583" TEXT="Recepci&#xf3;n de videos">
+<node CREATED="1303425501034" ID="ID_1951231778" MODIFIED="1303425507333" TEXT="Permiso del contribuyente">
+<node CREATED="1303425510720" ID="ID_1864314163" MODIFIED="1303425526803" TEXT="Estipulando que dona el video bajo CC o que hace release del video bajo CC"/>
+</node>
+<node CREATED="1303425529643" ID="ID_593043451" MODIFIED="1303425541586" TEXT="Sincronizar videos con audio de consola">
+<node CREATED="1303425544804" ID="ID_1779716916" MODIFIED="1303425620242" TEXT="Se remueve al audio original, y se agrega el audio de consola para la porci&#xf3;n del video que le corresponda"/>
+</node>
+<node CREATED="1303425623331" ID="ID_1876418424" MODIFIED="1303425628392" TEXT="Subir videos a youtube"/>
+<node CREATED="1303425628880" ID="ID_23740385" MODIFIED="1303425638042" TEXT="Agregar videos de youtube a p&#xe1;gina de proyecto"/>
+<node CREATED="1303426381099" ID="ID_1187096505" MODIFIED="1303426407330" TEXT="Anunciar via Facebook y a travez del grupo para que la gente entre a recomendar ediciones"/>
+</node>
+<node CREATED="1303425210583" ID="ID_1715537301" MODIFIED="1303425221193" TEXT="Edici&#xf3;n previa">
+<node CREATED="1303426616994" ID="ID_949825810" MODIFIED="1303426645461" TEXT="Se presenta una barra que representa el audio de cada track"/>
+<node CREATED="1303426647964" ID="ID_298063802" MODIFIED="1303426658149" TEXT="La barra del audio comienza vac&#xed;a"/>
+<node CREATED="1303426658889" ID="ID_1292202668" MODIFIED="1303426742176" TEXT="Los usuarios pueden agregar porciones del video, completando poco a poco el audio"/>
+<node CREATED="1303426745403" ID="ID_1060936390" MODIFIED="1303426780973" TEXT="Cuando hay varios cortes que completan una misma porci&#xf3;n de audio, se decide por votaci&#xf3;n o por los editores del proyecto"/>
+<node CREATED="1303426880573" ID="ID_1481025300" MODIFIED="1303426891898" TEXT="Cuando no hay video se pueden ingresar fotograf&#xed;as"/>
+</node>
+<node CREATED="1303425196471" ID="ID_1052594853" MODIFIED="1303425207680" TEXT="Edici&#xf3;n final"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1303424532406" ID="ID_1136696516" MODIFIED="1303424538741" POSITION="right" TEXT="Pruebas de sistema">
+<node CREATED="1303426489109" ID="ID_697117275" MODIFIED="1303426511980" TEXT="Primera versi&#xf3;n"/>
+<node CREATED="1303426512470" ID="ID_1468676675" MODIFIED="1303426523328" TEXT="Versi&#xf3;n para banda"/>
+</node>
+<node CREATED="1303424539154" ID="ID_301389734" MODIFIED="1303424546000" POSITION="right" TEXT="Grabaci&#xf3;n en producci&#xf3;n"/>
+<node CREATED="1327107990900" ID="ID_1252179246" MODIFIED="1327107999972" POSITION="left" TEXT="Implementaci&#xf3;n y desarrollo">
+<node CREATED="1327108023032" ID="ID_191501871" MODIFIED="1327108216618" TEXT="Sitio de github">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1327108045668" ID="ID_99360340" MODIFIED="1327108219481" TEXT="Creaci&#xf3;n de REAME, TODO">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1327108074818" ID="ID_861402118" MODIFIED="1327108221690" TEXT="Creaci&#xf3;n de mockup web">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1327108082642" ID="ID_228598802" MODIFIED="1327108224282" TEXT="Creaci&#xf3;n de video de demo-mockup de la aplicaci&#xf3;n">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1327108098918" FOLDED="true" ID="ID_1965355952" MODIFIED="1327108250018" TEXT="Sistema de plugins de video">
+<icon BUILTIN="full-5"/>
+<node CREATED="1327108126627" ID="ID_1120796470" MODIFIED="1327108136089" TEXT="Sitios">
+<node CREATED="1327108139155" ID="ID_882085648" MODIFIED="1327108151763" TEXT="Youtube CC"/>
+<node CREATED="1327108142280" ID="ID_782007672" MODIFIED="1327108154701" TEXT="Vimeo CC"/>
+</node>
+</node>
+<node CREATED="1327108158992" ID="ID_1410650658" MODIFIED="1327108252898" TEXT="Autenticaci&#xf3;n de usuario">
+<icon BUILTIN="full-6"/>
+<node CREATED="1327108172434" ID="ID_504785930" MODIFIED="1327108191900" TEXT="Se desea que sea sencillo y que no requiera registro">
+<node CREATED="1327108195275" ID="ID_550637211" MODIFIED="1327108210394" TEXT="OpenID">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1327108261835" ID="ID_1800623801" MODIFIED="1327108271922" TEXT="G+">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1327108266415" ID="ID_1148739280" MODIFIED="1327108269115" TEXT="FB">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1327108291716" ID="ID_225286004" MODIFIED="1327108589026" TEXT="Wizard para crear proyecto">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1327108308816" ID="ID_861918364" MODIFIED="1327108592314" TEXT="Interfaz de usuario de proyecto">
+<icon BUILTIN="full-8"/>
+<node CREATED="1327108320019" ID="ID_1780664322" MODIFIED="1327108325886" TEXT="Lista de videos"/>
+<node CREATED="1327108326590" ID="ID_1516892327" MODIFIED="1327108329660" TEXT="Canal de audio"/>
+<node CREATED="1327108330240" ID="ID_31995675" MODIFIED="1327108340937" TEXT="Lista de fotos"/>
+</node>
+<node CREATED="1327108360629" ID="ID_1520505142" MODIFIED="1327108594626" TEXT="Sincronizaci&#xf3;n de audio con videos">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1327108369869" ID="ID_1637259675" MODIFIED="1327108598410" TEXT="Beta p&#xfa;blico">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node CREATED="1327108511889" ID="ID_1843691227" MODIFIED="1327108601250" TEXT="Crear al menos 10 proyectos con conciertos de Carlos Delgado">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1327108546957" ID="ID_995073954" MODIFIED="1327108666258" TEXT="Limpiado pulgas y plugins para exportar">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1327108671392" ID="ID_1563485833" MODIFIED="1327108679966" TEXT="Exportar a DVD">
+<node CREATED="1327108689339" ID="ID_20271153" MODIFIED="1327108716057" TEXT="Da un torrent para bajar el DVD creado"/>
+</node>
+<node CREATED="1327108680761" ID="ID_751163557" MODIFIED="1327108685007" TEXT="Exportar a video en youtube"/>
+</node>
+<node CREATED="1327108561851" ID="ID_778974001" MODIFIED="1327108609066" TEXT="Versi&#xf3;n 1.0">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1327108568563" ID="ID_1052349962" MODIFIED="1327108612634" TEXT="Usar redes sociales para convocar a evento">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1327108622737" ID="ID_1457192320" MODIFIED="1327108632659" TEXT="Siguiente versi&#xf3;n"/>
+</node>
+</node>
+</map>
